8aff58f4b3dc9bae378a18d93f743f4a419ed5f2
patelpiyu255-png <patelpiyu255@gmail.com>
2026-08-13 23:25:49 +0530

    Day10 of array practice

---
+++ DSA/Arrays/Day10.cpp
@@ -0,0 +1,21 @@
+// Q.13. Write a C++ program to input temperature of 5 cities in an array and display the temperature in reverse order.
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int temp[5];
+	
+	cout<<"Enter the temp of 5 cities :\n ";
+	for (int i=0; i<5; i++)
+	{
+		cin>>temp[i];
+	}
+	
+	cout<<"temp in reverse order :\n";
+	for(int i=4; i>=0; i--)
+	{
+		cout<<temp[i] << " ";
+	}
+}
