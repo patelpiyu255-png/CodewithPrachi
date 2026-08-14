2ae200133b54289da2f27cfba92f374475ed7966
patelpiyu255-png <patelpiyu255@gmail.com>
2026-08-14 20:33:52 +0530

    swapping array

---
+++ DSA/Arrays/Day11.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    int start = 0;
+    int end = 4;
+
+    while (start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    cout << "Array after swapping: ";
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
