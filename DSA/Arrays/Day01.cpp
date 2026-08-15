eb616d2a0e1494ecc85f1e757575c0401b061dad
patelpiyu255-png <patelpiyu255@gmail.com>
2026-08-15 20:32:05 +0530

    count non zero element in array

--- DSA/Arrays/Day01.cpp
+++ DSA/Arrays/Day01.cpp
@@ -1,7 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-        cout<<"Hello world"<<endl;
-        cout<<"CodewithPrachi"<<endl;
+int main()
+{
+    int arr[7] = {1, 0, 3, 0, 5, 0, 2};
+    int n = 7;
+
+    int nonZeroCount = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != 0)
+        {
+            nonZeroCount++;
+        }
     }
+
+    cout << "Total non-zero elements = " << nonZeroCount;
+
+    return 0;
+}
