2458c56c858f9b458e736810b9918cb8371d681d
patelpiyu255-png <patelpiyu255@gmail.com>
2026-08-16 21:01:03 +0530

    count duplicate element

--- DSA/Arrays/Day13.cpp
+++ DSA/Arrays/Day13.cpp
@@ -3,20 +3,42 @@ using namespace std;
 
 int main()
 {
-    int arr[7] = {1, 2, 3, 2, 4, 1, 5};
+    int arr[7] = {1, 2, 2, 3, 1, 2, 4};
     int n = 7;
 
-    cout << "Duplicate elements: ";
+    cout << "Duplicate frequency:" << endl;
 
     for (int i = 0; i < n; i++)
     {
+        int count = 1;
+
+        bool alreadyCounted = false;
+
+        for (int k = 0; k < i; k++)
+        {
+            if (arr[i] == arr[k])
+            {
+                alreadyCounted = true;
+                break;
+            }
+        }
+
+        if (alreadyCounted)
+        {
+            continue;
+        }
+
         for (int j = i + 1; j < n; j++)
         {
             if (arr[i] == arr[j])
             {
-                cout << arr[i] << " ";
-                break;
+                count++;
             }
+        }
+
+        if (count > 1)
+        {
+            cout << arr[i] << " -> " << count << " times" << endl;
         }
     }
 
