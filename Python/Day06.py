21411f2afd185614d4813ca4b0c5228e872c3bca
patelpiyu255-png <patelpiyu255@gmail.com>
2026-08-19 20:58:41 +0530

    Add indexes to pair sum result

--- Python/Day06.py
+++ Python/Day06.py
@@ -6,4 +6,5 @@ for i in range(len(arr)):
     for j in range(i + 1, len(arr)):
 
         if arr[i] + arr[j] == target:
-            print(arr[i], "+", arr[j], "=", target)
+            print("Pair:", arr[i], "+", arr[j], "=", target)
+            print("Indexes:", i, "and", j)
